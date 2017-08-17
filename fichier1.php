b1fc5c5e351545421768f4d8fba758b562eda684
rekolarec <31099815+rekolarec@users.noreply.github.com>
2017-08-17 16:48:02 +0200

    zoubida

--- fichier1.php
+++ fichier1.php
@@ -2,3 +2,25 @@
 echo "Hello world!"
   echo"rajout de truc";
 echo"modif";
+
+// commande dans le shell
+
+// vérifier la bonne installation de git
+// git puis entrée
+
+// localisation du fichier d'emplacement
+// cd C:\xwamp\...
+
+// vérifier avec dir
+// dir puis entrée
+
+// vérifier la connexion 
+// git status 
+
+//
+// git add -A
+
+// préparer un fichier 
+// git commit -m
+
+// git push
